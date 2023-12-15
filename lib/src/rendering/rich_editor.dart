c4298c92c8f158b00a0f2eb5189b957e78025925
Ving Minear <jdoe@example.com>
2023-12-15 16:11:49 +0700

    add loading

--- lib/src/rendering/rich_editor.dart
+++ lib/src/rendering/rich_editor.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -89,6 +90,7 @@ class RichEditorState extends State<RichEditor> {
     );
   }
 
+  var loading = false;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -98,72 +100,89 @@ class RichEditorState extends State<RichEditor> {
           child: _buildToolBar(),
         ),
         Expanded(
-          child: Container(
-            decoration: widget.decoration ??
-                BoxDecoration(
-                  color: Colors.white,
-                ),
-            child: InAppWebView(
-              key: _mapKey,
-              onWebViewCreated: (controller) async {
-                _controller = controller;
-                setState(() {});
-                if (!kIsWeb && !Platform.isAndroid) {
-                  await _loadHtmlFromAssets();
-                } else {
-                  await _controller!.loadUrl(
-                    urlRequest: URLRequest(
-                      url: Uri.tryParse(
-                          'file:///android_asset/flutter_assets/$assetPath'),
+          child: loading
+              ? Center(
+                  child: CircularProgressIndicator(
+                  strokeWidth: 2.5,
+                ))
+              : Container(
+                  decoration: widget.decoration ??
+                      BoxDecoration(
+                        color: Colors.white,
+                      ),
+                  child: InAppWebView(
+                    key: _mapKey,
+                    onLoadResource: (controller, resource) {
+                      log("message");
+                    },
+                    onLoadStart: (controller, url) async {
+                      loading = true;
+                      setState(() {});
+                      await controller.isLoading().then((value) {
+                        loading = false;
+                        setState(() {});
+                      });
+                    },
+                    onWebViewCreated: (controller) async {
+                      _controller = controller;
+                      setState(() {});
+                      if (!kIsWeb && !Platform.isAndroid) {
+                        await _loadHtmlFromAssets();
+                      } else {
+                        await _controller!.loadUrl(
+                          urlRequest: URLRequest(
+                            url: Uri.tryParse(
+                                'file:///android_asset/flutter_assets/$assetPath'),
+                          ),
+                        );
+                      }
+                    },
+                    initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        transparentBackground: true,
+                        javaScriptEnabled: true,
+                        useOnLoadResource: true,
+                        clearCache: true,
+                        disableHorizontalScroll:
+                            widget.horizontalScroll ?? true,
+                        supportZoom: widget.zoom ?? false,
+                        preferredContentMode: UserPreferredContentMode.MOBILE,
+                      ),
                     ),
-                  );
-                }
-              },
-              initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(
-                  transparentBackground: true,
-                  javaScriptEnabled: true,
-                  useOnLoadResource: true,
-                  clearCache: true,
-                  disableHorizontalScroll: widget.horizontalScroll ?? true,
-                  supportZoom: widget.zoom ?? false,
-                  preferredContentMode: UserPreferredContentMode.MOBILE,
-                ),
-              ),
 
-              onPageCommitVisible: (controller, url) {
-                controller.evaluateJavascript(source: """
+                    onPageCommitVisible: (controller, url) {
+                      controller.evaluateJavascript(source: """
                 var style = document.createElement('style');
                 style.innerHTML = "img {border:none;max-width: auto; height: auto;object-fit: contain;pointer-events: none;user-select: none; touch-action: none; touch-action: pan-x pan-y;  overflow: hidden;}"
                 document.head.appendChild(style);
             """);
-              },
-              onLoadStop: (controller, link) async {
-                if (link!.path != 'blank') {
-                  javascriptExecutor.init(_controller!);
-                  await _setInitialValues();
-                  _addJSListener();
-                }
-              },
+                    },
+                    onLoadStop: (controller, link) async {
+                      if (link!.path != 'blank') {
+                        javascriptExecutor.init(_controller!);
+                        await _setInitialValues();
+                        _addJSListener();
+                      }
+                    },
 
-              // javascriptMode: JavascriptMode.unrestricted,
-              // gestureNavigationEnabled: false,
-              gestureRecognizers: widget.gestureRecognizers ??
-                  [
-                    Factory(() =>
-                        VerticalDragGestureRecognizer()..onUpdate = (_) {}),
-                    Factory(() => HorizontalDragGestureRecognizer()),
-                  ].toSet(),
-              onLoadError: (controller, url, code, e) {
-                print("error $e $code");
-              },
-              onConsoleMessage: (controller, consoleMessage) async {
-                print(
-                  'WebView Message: $consoleMessage',
-                );
-              },
-            ),
-          ),
+                    // javascriptMode: JavascriptMode.unrestricted,
+                    // gestureNavigationEnabled: false,
+                    gestureRecognizers: widget.gestureRecognizers ??
+                        [
+                          Factory(() => VerticalDragGestureRecognizer()
+                            ..onUpdate = (_) {}),
+                          Factory(() => HorizontalDragGestureRecognizer()),
+                        ].toSet(),
+                    onLoadError: (controller, url, code, e) {
+                      print("error $e $code");
+                    },
+                    onConsoleMessage: (controller, consoleMessage) async {
+                      print(
+                        'WebView Message: $consoleMessage',
+                      );
+                    },
+                  ),
+                ),
         ),
         Visibility(
           visible: widget.editorOptions!.barPosition == BarPosition.BOTTOM,
